76f49aa5bdc36771fe10d47a2019b01e42c0dd2b
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-05 10:24:19 +0200

    cvc

---
+++ main.js
@@ -0,0 +1,67 @@
+// 'use strict'    // mod restrittiva. can applicare a tutto il file o a singole funzioni
+
+let myName
+
+function setName(newName) {
+        
+    myNam = newName
+
+}
+
+setName('Walter') // Error!!!
+console.log('myName:'+ myName)
+console.log('myNam:'+ myNam)
+
+
+//  Annotazioni di tipo
+
+//val: bool
+//bool -> bool  // entra un bool esce un bool
+function invert(val) {
+    return !val
+}
+
+console.log('invert(true):' + invert(true))
+console.log('invert5):' + invert(5))
+
+// (int,int) -> int
+
+function multiply(n1,n2){
+    return n1*n2
+}
+
+// Tipi generici
+
+//Array[T] -> T
+
+// pickFirstElement(array){
+//     return array[0];
+// }
+
+// pickFirstElement(['ciao','mondo']) // -> 'ciao'
+pickFirstElementOfArray([1,2,3])// -> 1
+
+
+function test(testDescription, body) {
+    if (!body()) {
+    console.log(`TEST SUCCEEDED: ${testDescription}`);
+  }else{
+    console.log(`TEST FAILED: ${testDescription}`)
+  }
+  }
+
+
+
+test('Picking first element of array of strings' ,function(){
+return pickFirstElementOfArray(['ciao','mondo']) ==='ciao'})
+
+test('Picking first element of array of integers' ,function(){
+    return pickFirstElementOfArray([1,2,3]) ===1}) 
+
+
+    function repeatStr (n, s) {
+ 
+    for (let i = 0; i <= n; i++) {
+        
+    }
+}
